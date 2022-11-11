3ec318a96f75f4cddd96290d8c05004f17e7fc52
Ben Adams <emailbenjaminadams@gmail.com>
2022-11-11 16:22:59 +0000

    password

---
+++ Classes/IPassword.cs
@@ -0,0 +1,13 @@
+﻿namespace MoodJournal.Security
+{
+    public interface IPassword
+    {
+        string EncryptedPassword { get; set; }
+        string Password { get; set; }
+        string Salt { get; set; }
+        void Encrypt(string plainTextPass);
+        void Encrypt(string plainTextPass, string Salt);
+        void Decrypt(string plainTextPass);
+        void Decrypt(string plainTextPass, string Salt);
+    }
+}
---
+++ Classes/Password.cs
@@ -0,0 +1,36 @@
+﻿namespace MoodJournal.Security
+{
+    public class Password : MoodJournal.Security.IPassword
+    {
+        IPassword _ipassword;
+
+        public Password()
+        {
+            _ipassword = this;
+        }
+
+        public string EncryptedPassword { get; set; }
+        public string Salt { get; set; }
+        string IPassword.Password { get; set; }
+
+        public void Decrypt(string plainTextPass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Decrypt(string plainTextPass, string Salt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Encrypt(string plainTextPass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Encrypt(string plainTextPass, string Salt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
